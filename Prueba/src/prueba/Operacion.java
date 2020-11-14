2325436dec6ec3445c9d6a686dca8eb69238b43e
Daniel <eduardomontero123@hotmail.com>
2020-11-14 17:34:42 -0500

    Cuarto

--- Prueba/src/prueba/Operacion.java
+++ Prueba/src/prueba/Operacion.java
@@ -5,10 +5,21 @@
  */
 package prueba;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pc
  */
 public class Operacion {
-    
+    int num1, num2, res;
+    Scanner tecla = new Scanner(System.in);
+    public void Resta (){
+        System.out.println("Ingrese el primer número: ");
+        num1 = Integer.valueOf(tecla.nextLine());
+        System.out.println("Ingrese el segundo número: ");
+        num2 = Integer.valueOf(tecla.nextLine());
+        res = num1 - num2;
+        System.out.print("Respuesta: " + res);
+    }
 }
